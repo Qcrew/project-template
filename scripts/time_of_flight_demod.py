0ac44b16b81f97e5f3934f0f7f218cf0bcc585c4
Atharv Joshi <atharvjoshi@ymail.com>
2023-07-28 19:42:55 +0800

    Correct shape of adc demod

--- scripts/time_of_flight_demod.py
+++ scripts/time_of_flight_demod.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
         datafn_args={"freq": RR.int_freq, "length": READOUT_PULSE.total_length},
         plot_args={"plot_type": "line", "plot_err": False},
     )
-    ADC_DEMOD.initialize(axes=[2, READOUT_PULSE.total_length])  # 1: real, 2: imag
+    ADC_DEMOD.initialize(axes=[N, 2, READOUT_PULSE.total_length])  # 1: real, 2: imag
     datasets = [ADC, ADC_FFT, ADC_DEMOD]
 
     ######################## INITIALIZE AND RUN EXPERIMENT #############################
